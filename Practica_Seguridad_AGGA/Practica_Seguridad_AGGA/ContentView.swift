c21b2e9671ae04d8b1d3c133cdd9d2798e8dfef6
Alejandro Alberto Gavira García <alex.gavira@gmail.com>
2024-05-10 19:21:43 +0200

    Init proyect

---
+++ Practica_Seguridad_AGGA/Practica_Seguridad_AGGA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Practica_Seguridad_AGGA
+//
+//  Created by Alejandro Alberto Gavira García on 10/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
