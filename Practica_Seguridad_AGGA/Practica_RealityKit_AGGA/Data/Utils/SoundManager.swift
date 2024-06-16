cea6674432d0b89089f4de857262da840bc92790
AlejandroGaviraGarcia <alejandro_gavira@epam.com>
2024-06-16 17:59:33 +0200

    Finish project. Added fginal view + mini game capture, Launch screen, Sounds, Try rthe raycast hitpoints. Changed the stadiuym to add the 9 first pokemons.

---
+++ Practica_Seguridad_AGGA/Practica_RealityKit_AGGA/Data/Utils/SoundManager.swift
@@ -0,0 +1,8 @@
+//
+//  SoundManager.swift
+//  Practica_RealityKit_AGGA
+//
+//  Created by Alejandro Gavira on 16/6/24.
+//
+
+import Foundation
---
+++ Practica_Seguridad_AGGA/Practica_RealityKit_AGGA/Presentation/View/FinalView/FinalView.swift
@@ -0,0 +1,18 @@
+//
+//  FinalView.swift
+//  Practica_RealityKit_AGGA
+//
+//  Created by Alejandro Gavira on 16/6/24.
+//
+
+import SwiftUI
+
+struct FinalView: View {
+    var body: some View {
+        Image()
+    }
+}
+
+#Preview {
+    FinalView()
+}
